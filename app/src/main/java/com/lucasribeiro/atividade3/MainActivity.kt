0fec0cb099e3518c40c84553a6ac5f12383cf096
Lucas Ribeiro <lrpessamilio@gmail.com>
2021-06-25 12:23:15 -0300

    Created calculate function

--- app/src/main/java/com/lucasribeiro/atividade3/MainActivity.kt
+++ app/src/main/java/com/lucasribeiro/atividade3/MainActivity.kt
@@ -2,10 +2,50 @@ package com.lucasribeiro.atividade3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
+    var n1TextField: EditText? = null
+    var n2TextField: EditText? = null
+    var radioGrupoOperation: RadioGroup? = null
+    var resultView: TextView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        n1TextField = findViewById<EditText>(R.id.n1TextField)
+        n2TextField = findViewById<EditText>(R.id.n2TextField)
+        radioGrupoOperation = findViewById<RadioGroup>(R.id.radioGroupOperation)
+        resultView = findViewById<TextView>(R.id.resultText)
     }
+
+    fun calcular(){
+        val n1String = n1TextField!!.text.toString()
+        val n2String = n2TextField!!.text.toString()
+
+        if (n1String.trim() != "" && n2String.trim() != ""){
+            val n1 = n1String.toDouble()
+            val n2 = n2String.toDouble()
+
+            when(radioGrupoOperation!!.checkedRadioButtonId){
+                R.id.radioButtonPlus-> resultView!!.text = (n1 + n2).toString()
+                R.id.radioButtonMinus-> resultView!!.text = (n1 - n2).toString()
+                R.id.radioButtonTimes-> resultView!!.text = (n1 * n2).toString()
+                R.id.radioButtonDivided-> {
+                    if(n2 == 0.0) resultView!!.text = ""
+                    else resultView!!.text = (n1/n2).toString()
+                }
+            }
+        } else resultView!!.text = ""
+    }
+
+
 }
